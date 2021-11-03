bfd5c35482ab2bbdd1392d6b7c9b33780fed0fde
jan <janek.grochulski@gmail.com>
2021-11-03 11:36:58 -0500

    fin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+// // Code your solution here
+
+function findMatching (array, target) { 
+    return array.filter(name => name.toLowerCase() === target.toLowerCase());
+}
+
+function fuzzyMatch (array, target) {
+    return array.filter(name => name.toLowerCase().slice(0,target.length) === target.toLowerCase());
+}
+
+function matchName (obj, target) { 
+    return obj.filter(element => element.name.toLowerCase() === target.toLowerCase());
+}
